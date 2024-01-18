736967f93e1a5df1601c482828840dfb45d6442b
subhajitbhukta <143212971+subhajitbhukta@users.noreply.github.com>
2024-01-18 20:53:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let input=document.getElementById('inputBox');
+let buttons=document.querySelectorAll('button');
+
+let string="";
+let arr=Array.from(buttons);
+
+arr.forEach(button=>{
+button.addEventListener('click',(e)=>{
+    if(e.target.innerHTML == '='){
+        string = eval(string);
+        input.value = string;
+    }
+    else if(e.target.innerHTML == 'Clr'){
+        string = "";
+        input.value = string;
+    } else if(e.target.innerHTML == 'X'){
+        string=string.substring(0,string.length-1);
+        input.value = string;
+    }else if(e.target.innerHTML == 'S'){
+        // document.body.style.backgroundColor="green";
+        string="Subhajit  ";
+        input.value = string;
+    }
+    else{
+        string += e.target.innerHTML;
+        input.value=string;
+    }
+})
+})
